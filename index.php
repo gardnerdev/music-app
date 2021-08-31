f7a1e8dcea71f66a3bbafcf7f64ec894b892b7c2
Rafał Chojnacki <rafalchojnacki0502@gmail.com>
2021-08-31 15:35:00 +0200

    added footer and header

--- index.php
+++ index.php
@@ -1,20 +1,30 @@
 <?php
    
-
+    define('NAME', 'Uoshi');
+    $fileName = "Tamagotchi";
 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+    <?php include('templates/header.php')?>
+    <?php include('templates/footer.php')?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Music app</title>
+    <title>Music songs</title>
 </head>
 <body>
         <h1><?php echo 'hello, music app'; ?></h1>
-    
+        <div>
+            <?php
+            echo $fileName;
+            ?>
+        </div>
+        <div>
+            <?php echo NAME ?>
+        </div>
 </body>
 </html>
---
+++ templates/header.php
@@ -0,0 +1,23 @@
+<head>
+    <title>
+        Music songs portal
+    </title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style type="text/css">
+        .brand-text{
+            color: #14a85c !important;
+        }
+    </style>
+</head>
+<body class="grey lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo brand-text center">Music songs portal</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li>
+                    <a href="#" class="btn brand z-depth-0">Add a song</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
